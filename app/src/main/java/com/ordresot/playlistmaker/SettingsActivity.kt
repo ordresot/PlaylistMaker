f2efcdbf3234d0f14bc44a255bf4c30300d7bfe5
ordesot <derisheff2014@yandex.ru>
2024-11-11 23:39:50 +0300

    Homework 1 and 2

--- app/src/main/java/com/ordresot/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/ordresot/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.ordresot.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
@@ -7,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,33 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar = findViewById<MaterialToolbar>(R.id.activity_settings_toolbar)
         toolbar.setNavigationOnClickListener{
             finish()
+        }
+
+        val shareElement = findViewById<MaterialTextView>(R.id.shareElement)
+        shareElement.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.link_to_android_developer_course))
+                type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+
+        val supportElement = findViewById<MaterialTextView>(R.id.supportElement)
+        supportElement.setOnClickListener{
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text))
+            }
+            startActivity(emailIntent)
+        }
+
+        val userAgreement = findViewById<MaterialTextView>(R.id.userAgreement)
+        userAgreement.setOnClickListener{
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_to_user_agreement)))
+            startActivity(browserIntent)
         }
     }
 }
