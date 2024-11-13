e3fcdfe8ee90772afbca531637a747353337239b
ordesot <derisheff2014@yandex.ru>
2024-11-13 21:21:59 +0300

    Small changes

--- app/src/main/java/com/ordresot/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/ordresot/playlistmaker/SearchActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
 
@@ -28,6 +29,11 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.activity_search_toolbar)
+        toolbar.setNavigationOnClickListener{
+            finish()
+        }
 
         val searchField = findViewById<EditText>(R.id.search_field)
         val clearButton = findViewById<ImageView>(R.id.clear_button)
