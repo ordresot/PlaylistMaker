481da0895e2317e0cf9ae1cb2f47993e9d748004
ordesot <derisheff2014@yandex.ru>
2024-11-13 20:01:35 +0300

    Homework 4

--- app/src/main/java/com/ordresot/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/ordresot/playlistmaker/SearchActivity.kt
@@ -16,6 +16,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchText = SEARCH_TEXT_DEF
+
+    companion object{
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val SEARCH_TEXT_DEF = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -38,6 +46,7 @@ class SearchActivity : AppCompatActivity() {
         searchField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+                searchText = s.toString()
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -51,5 +60,17 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT, SEARCH_TEXT_DEF)
+        val searchField = findViewById<EditText>(R.id.search_field)
+        searchField.setText(searchText)
     }
 }
